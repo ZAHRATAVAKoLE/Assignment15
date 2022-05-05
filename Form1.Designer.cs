8540b05fbb0db8d3b10f2f4a7cb0ae0d95a1847a
ZahraTavakoli <100377681+ZAHRATAVAKoLE@users.noreply.github.com>
2022-05-05 10:23:36 +0430

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace sodoco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.OutsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 9;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 11);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.23937F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.23937F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.23937F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.66221F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.66221F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.23937F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.23937F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.23937F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.23937F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(422, 381);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.BackgroundImage = global::sodoco.Properties.Resources.Untitledl;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Maroon;
+            this.button3.Location = new System.Drawing.Point(265, 396);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "check";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.BackColor = System.Drawing.Color.MintCream;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Image = global::sodoco.Properties.Resources.Untitledkkk;
+            this.button1.Location = new System.Drawing.Point(57, 396);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New Game";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.BackgroundImage = global::sodoco.Properties.Resources.Untitledl;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(453, 455);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace sodoco
+{
+    public partial class Form1 : Form
+    {
+        TextBox[,] cells=new TextBox[9,9];
+        
+        String[,] b = new String[9, 9];
+        public Form1()
+        {
+            InitializeComponent(); 
+            cells = new TextBox[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    cells[i, j] = new TextBox();
+
+                    cells[i, j].Multiline = true;
+                    cells[i, j].TextAlign = HorizontalAlignment.Center;
+                    cells[i, j].Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right | AnchorStyles.Left;
+                    cells[i, j].Font = new Font("Tahoma", 15);
+                    tableLayoutPanel1.Controls.Add(cells[i, j], i, j);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            OpenFileDialog x = new OpenFileDialog();
+            if (x.ShowDialog() == DialogResult.OK)
+            {
+                reset();
+                string fiie_path = x.FileName;
+                StreamReader my_file_reder = new StreamReader(fiie_path);
+                string big_text = my_file_reder.ReadToEnd();
+               
+                char[] my_sep = { ' ',  '\r' };
+                big_text = big_text.Replace("\n","");
+                string[] numbers = big_text.Split(my_sep);
+
+                int index = 0;
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (numbers[index] != "0")
+                        {
+                            cells[j, i].ReadOnly = true;
+                            cells[j, i].Text = numbers[index];
+                            cells[j, i].BackColor = Color.LightYellow;
+
+
+
+
+
+
+                        }
+                        index++;   
+                    }
+                }
+            }
+        }
+        private void cells_TextChanged(object sender, EventArgs e)
+        { }
+        private void reset()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                   
+                    {
+                        cells[j, i].ReadOnly = false;
+                        cells[j, i].Text = "";
+                        cells[i, j].TextChanged += new System.EventHandler(this.cells_TextChanged);
+                        
+
+                    }
+                    
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+     
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int list;
+            int[] row;
+            int[] colom;
+            int[] sq;
+           
+            for (int i = 0; i < 9; i++)
+            {
+                row = new int[9];
+                colom = new int[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    if (cells[i, j].Text != "" && cells[i, j].Text != "")
+                    {
+                        int cellrow = Convert.ToInt16(cells[j, i].Text);
+                        int cellcolom = Convert.ToInt16(cells[i, j].Text);
+                        if (0 < cellrow && cellrow < 10 && !row.Contains(cellrow) &&
+                             0 < cellcolom && cellcolom < 10 && !colom.Contains(cellcolom))
+                        {
+                            row[j] = cellrow;
+                            colom[i] = cellcolom;
+                            continue;
+                        }
+                    }
+                    MessageBox.Show("TRY AGIAN ");
+                    return;
+                }
+            }
+            for (int I = 1,J1 = 0, c1 = 3, J2 = 0, c2 = 3; I<= 9; I++, J2 += 3, c2 += 3)
+
+            {
+
+                if ((I - 1) % 3 == 0)
+
+                {
+                   J2 = 0;
+                    c2 = 3; 
+                }
+                sq = new int[9];
+                list = 0;
+                
+                for (int i = J1; i < c1; i++)
+
+                {
+                    for (int j = J2; j < c2; j++)
+
+                    {
+                        int cell = Convert.ToInt16(cells[j, i].Text);
+                        
+                        if (!sq.Contains(cell))
+
+                        {
+                            sq[list++] = cell;
+                       
+                            continue;
+                        }
+                        
+                        MessageBox.Show("TRY AGAIN");
+
+                        return;
+
+                    }
+
+                }
+                
+                if (I % 3 == 0)
+
+                {
+                    J1 += 3;
+
+                    c1 += 3;
+                }
+            }
+            
+            MessageBox.Show("YOU WIN");
+        }
+
+     
+    }
+}
+
